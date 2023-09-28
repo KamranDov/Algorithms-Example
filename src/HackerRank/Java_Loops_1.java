ba7458cd117b9b990b3646e4352d7e24d0e50563
Kamran Ahmadov <axmedovkamran19@gmail.com>
2023-09-28 13:43:48 +0400

    Loop 1 example added

--- src/HackerRank/Java_Loops_1.java
+++ src/HackerRank/Java_Loops_1.java
@@ -11,10 +11,7 @@ public class Java_Loops_1 {
         int N = Integer.parseInt(bufferedReader.readLine().trim());
 
         for (int i = 1; i <= 10; i++) {
-            if (i <=10) {
-                int result = i * N;
-                System.out.println(N + " x " + i + " = " + result);
-            }
+                System.out.println(N + " x " + i + " = " + N * i);
         }
         System.out.println();
 
