de98e0bb5cfbee9f5a8d7e8e814be8ae59cb0d01
Kamran Ahmadov <axmedovkamran19@gmail.com>
2023-10-01 16:59:08 +0400

    IfElse

--- src/HackerRank/Java_IfElse.java
+++ src/HackerRank/Java_IfElse.java
@@ -3,7 +3,6 @@ package HackerRank;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class Java_IfElse {
     public static void main(String[] args) throws IOException {
