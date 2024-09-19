77e15221bcfb36cbb54ff8e1aecf5112356ebb2a
Kaman Ahmadov <kamrana@crocusoft.com>
2024-09-19 10:58:12 +0400

    add new algorithm example (Problem Sloving - A Very Big Sum)

---
+++ src/HackerRank/ProblemSolving/AVeryBigSum.java
@@ -0,0 +1,25 @@
+package HackerRank.ProblemSolving;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class AVeryBigSum {
+
+
+    public static long aVeryBigSum(List<Long> ar) {
+
+        AtomicLong sum = new AtomicLong();
+
+        ar.forEach(sum::addAndGet);
+//        ar.forEach(param-> sum.addAndGet(param));
+
+        return sum.get();
+    }
+
+    public static void main(String[] args) {
+
+        List<Long> ar = List.of(1000000001L, 1000000002L, 1000000003L, 1000000004L, 1000000005L);
+        System.out.println(aVeryBigSum(ar));
+
+    }
+}
